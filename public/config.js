0e20a068b433715a3137df5a3a13ca812616635d
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-04-13 06:22:07 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -14,6 +14,7 @@ window.MARUWELL_CONFIG = {
     dragon_car_index : "/dragon_car_index.html",
     maroowell_info : "/maroowell_info.html",
     dragon_car_pay : "/dragon_car_pay.html",
-    maroowell_payout : "/maroowell_payout"
+    maroowell_payout : "/maroowell_payout",
+    maroowell_route : "/maroowell_route"
   }
 };

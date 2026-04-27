f1fb5ddf95a16a82d16e66798829acdc27ce83ca
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-04-27 14:37:08 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,18 +4,18 @@
 
 window.MARUWELL_CONFIG = {
   SUPABASE_URL: "https://rgqerimdxkthkcewqbbe.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJncWVyaW1keGt0aGtjZXdxYmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNjI4NTMsImV4cCI6MjA4MDgzODg1M30.3BXLU213Rt3JtSLn1ToEacj70lSSGfEWyYFezofYeyw",
+  SUPABASE_ANON_KEY: "sb_publishable_FUFuH5JVyM-JLWWVeasgOw_Sk_LtD9H",
+  ADMIN_API_BASE: "https://admin-access.maroowell.com",
 
-  // 경로(확장자 없는 URL 기준)
   PATHS: {
     login: "/login.html",
     index: "/index.html",
     route: "/coupangRouteMap.html",
-    dragon_car_index : "/dragon_car_index.html",
-    maroowell_info : "/maroowell_info.html",
-    dragon_car_pay : "/dragon_car_pay.html",
-    maroowell_payout : "/maroowell_payout",
-    maroowell_route : "/maroowell_route",
-    admin_access : "/admin_access.html
+    dragon_car_index: "/dragon_car_index.html",
+    maroowell_info: "/maroowell_info.html",
+    dragon_car_pay: "/dragon_car_pay.html",
+    maroowell_payout: "/maroowell_payout",
+    maroowell_route: "/maroowell_route",
+    admin_access: "/admin_access.html"
   }
 };

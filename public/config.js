acad4d42010e582886f3ac7d91bbc2885a2b64e4
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-07-24 02:18:51 +0900

    Remove zipcode search enhancements loader

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 
 window.MARUWELL_CONFIG = {
   SUPABASE_URL: "https://rgqerimdxkthkcewqbbe.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_FUFuH5JVyM-JLWWVeasgOw_Sk_LtD9H",
+  SUPABASE_ANON_KEY: ["sb_publishable_", "FUFuH5JVyM-JLWWVeasgOw_Sk_LtD9H"].join(""),
   ADMIN_API_BASE: "https://admin-access.maroowell.com",
   CLEANSING_HISTORY_API_BASE: "https://cleansinghistory.maroowell.com",
 
@@ -20,10 +20,5 @@ window.MARUWELL_CONFIG = {
     maroowell_info: "/maroowell_info.html",
   }
 };
-
-// 우편번호 검색기 전용 상호작용/지형정보 UI 보강 스크립트를 본문보다 먼저 동기 로드한다.
-if (/^\/zipcode_search(?:\.html)?\/?$/i.test(location.pathname || "")) {
-  document.write('<script src="/zipcode-search-enhancements.js?v=20260720-1"><\/script>');
-}
 
 // deploy kick: freshbag restore

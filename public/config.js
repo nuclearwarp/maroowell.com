5549bbd6b6a8d676de120c1e643572c24b550791
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-09-25 21:59:59 +0900

    Use cache-isolated route map path

--- public/config.js
+++ public/config.js
@@ -31,7 +31,7 @@ window.MARUWELL_CONFIG = {
   PATHS: {
     login: "/",
     index: "/post_login",
-    route: "/coupangRouteMap-v20260925.html",
+    route: "/route-map-20260925.html",
     dragon_car_index: "/dragon_car_index.html",
     maroowell_info: "/maroowell_info.html"
   }

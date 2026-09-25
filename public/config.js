b21e3c397dcdfc58e53f5142903be5b0f90c998d
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-09-25 21:57:38 +0900

    Bust route map cache with versioned path

--- public/config.js
+++ public/config.js
@@ -31,7 +31,7 @@ window.MARUWELL_CONFIG = {
   PATHS: {
     login: "/",
     index: "/post_login",
-    route: "/coupangRouteMap.html",
+    route: "/coupangRouteMap-v20260925.html",
     dragon_car_index: "/dragon_car_index.html",
     maroowell_info: "/maroowell_info.html"
   }
